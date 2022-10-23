edf48c51e1b0ad09b233be7d1642feaf7d64ed35
youss95 <usekim7555@gmail.com>
2022-10-23 22:50:02 +0900

    init

---
+++ src/test/java/com/ksy/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
